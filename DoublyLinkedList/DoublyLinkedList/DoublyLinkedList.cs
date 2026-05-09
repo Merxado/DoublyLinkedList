20d90c806d5c05cc2aed81e929f7383f34f540ec
Merxado <mayimamiyyo1112@gmail.com>
2026-05-09 18:10:10 -0500

    Add list

---
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class DoublyLinkedList<T> where T : IComparable<T>
+{
+    public class DoublyLinkedList<T> where T : IComparable<T>
+    {
+        private DoubleNode<T> head;
+        private DoubleNode<T> tail;
+
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void Insert(T data)
+        {
+            DoubleNode<T> newNode = new DoubleNode<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+
+            if (data.CompareTo(head.Data) <= 0)
+            {
+                newNode.Next = head;
+                head.Prev = newNode;
+                head = newNode;
+                return;
+            }
+
+            DoubleNode<T> current = head;
+            while (current.Next != null && current.Next.Data.CompareTo(data) <= 0)
+            {
+                current = current.Next;
+            }
+
+            newNode.Next = current.Next;
+            newNode.Prev = current;
+
+            if (current.Next != null)
+                current.Next.Prev = newNode; // hay nodo después
+            else
+                tail = newNode;              // era el último, actualizar tail
+
+            current.Next = newNode;
+        }
+    }
+}
+
--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+
+        list.Insert("Blanco");
+        list.Insert("Azul");
+        list.Insert("Amarillo");
+        list.Insert("Verde");
+        list.Insert("Negro");
+
+        list.PrintForward();
+        list.PrintBackward();
+
+        list.Remove("Amarillo");
+        list.PrintForward();
+    }
+}
