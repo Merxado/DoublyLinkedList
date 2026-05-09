6873b27660f7413286931a7d110a10d9dcbad931
Merxado <mayimamiyyo1112@gmail.com>
2026-05-09 18:36:15 -0500

    Add Menu to List

--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -1,13 +1,45 @@
-﻿static void Main(string[] args)
+﻿namespace LinkedLists
 {
-    DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+            int opcion;
 
-    list.Insert("Blanco");
-    list.Insert("Azul");
-    list.Insert("Amarillo");
-    list.Insert("Verde");
-    list.Insert("Negro");
+            do
+            {
+                Console.WriteLine("\n===== MENÚ =====");
+                Console.WriteLine("1. Adicionar");
+                Console.WriteLine("2. Mostrar hacia adelante");
+                Console.WriteLine("3. Mostrar hacia atrás");
+                Console.WriteLine("0. Salir");
+                Console.Write("Opción: ");
+                opcion = int.Parse(Console.ReadLine());
 
-    list.PrintForward();
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese un valor: ");
+                        string valor = Console.ReadLine();
+                        list.Insert(valor);
+                        break;
+                    case 2:
+                        list.PrintForward();
+                        break;
+                    case 3:
+                        list.PrintBackward();
+                        break;
+                    case 0:
+                        Console.WriteLine("Saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+            } while (opcion != 0);
+        }
+    }
 }
+
 
