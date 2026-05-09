b7679815b27540460a132b38c45daab96614ed87
Merxado <mayimamiyyo1112@gmail.com>
2026-05-09 18:19:26 -0500

    Add PrintForward

--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -1,53 +1,62 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class DoublyLinkedList<T> where T : IComparable<T>
 {
-    public class DoublyLinkedList<T> where T : IComparable<T>
+    private DoubleNode<T> head;
+    private DoubleNode<T> tail;
+
+    public DoublyLinkedList()
     {
-        private DoubleNode<T> head;
-        private DoubleNode<T> tail;
+        head = null;
+        tail = null;
+    }
 
-        public DoublyLinkedList()
+    public void Insert(T data)
+    {
+        DoubleNode<T> newNode = new DoubleNode<T>(data);
+
+        if (head == null)
         {
-            head = null;
-            tail = null;
+            head = newNode;
+            tail = newNode;
+            return;
         }
 
-        public void Insert(T data)
+        if (data.CompareTo(head.Data) <= 0)
         {
-            DoubleNode<T> newNode = new DoubleNode<T>(data);
-
-            if (head == null)
-            {
-                head = newNode;
-                tail = newNode;
-                return;
-            }
-
-            if (data.CompareTo(head.Data) <= 0)
-            {
-                newNode.Next = head;
-                head.Prev = newNode;
-                head = newNode;
-                return;
-            }
-
-            DoubleNode<T> current = head;
-            while (current.Next != null && current.Next.Data.CompareTo(data) <= 0)
-            {
-                current = current.Next;
-            }
-
-            newNode.Next = current.Next;
-            newNode.Prev = current;
-
-            if (current.Next != null)
-                current.Next.Prev = newNode; // hay nodo después
-            else
-                tail = newNode;              // era el último, actualizar tail
-
-            current.Next = newNode;
+            newNode.Next = head;
+            head.Prev = newNode;
+            head = newNode;
+            return;
         }
+
+        DoubleNode<T> current = head;
+        while (current.Next != null && current.Next.Data.CompareTo(data) <= 0)
+        {
+            current = current.Next;
+        }
+
+        newNode.Next = current.Next;
+        newNode.Prev = current;
+
+        if (current.Next != null)
+            current.Next.Prev = newNode; // hay nodo después
+        else
+            tail = newNode;              // era el último, actualizar tail
+
+        current.Next = newNode;
+
+    }
+    public void PrintForward()
+    {
+        DoubleNode<T> current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " <-> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
     }
 }
 
--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -1,19 +1,13 @@
-﻿class Program
+﻿static void Main(string[] args)
 {
-    static void Main(string[] args)
-    {
-        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+    DoublyLinkedList<string> list = new DoublyLinkedList<string>();
 
-        list.Insert("Blanco");
-        list.Insert("Azul");
-        list.Insert("Amarillo");
-        list.Insert("Verde");
-        list.Insert("Negro");
+    list.Insert("Blanco");
+    list.Insert("Azul");
+    list.Insert("Amarillo");
+    list.Insert("Verde");
+    list.Insert("Negro");
 
-        list.PrintForward();
-        list.PrintBackward();
-
-        list.Remove("Amarillo");
-        list.PrintForward();
-    }
+    list.PrintForward();
 }
+
